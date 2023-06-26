4e8a95d67cab4750d4ca591e53269c0b180fbec6
Venkatesh <122005350+Venkatesh-KVS@users.noreply.github.com>
2023-06-26 04:58:47 +0000

    commit on 10:27 26th

--- src/styles/Styled.Global.js
+++ src/styles/Styled.Global.js
@@ -1,7 +1,28 @@
-import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
 
-export const StyledGlobal = styled.div`
+export const StyledGlobal = createGlobalStyle`
+  
+  body {
+    margin: 0;
+    padding: 0; 
+  }
+  .container {
+    padding: 0;
+    max-width: 75rem;
+  }
   h2 {
-    color: red;
+    font-size: 2rem;
+    font-weight: 700;
+  }
+  .para {
+    font-size: 1rem;
+    color: #00203c;
+    margin: 0;
+  }
+  .paraH3 {
+    font-size: 1.5rem;
+    font-weight: 700;
+    /* color: #00203c; */
+    margin-bottom: 1.5rem;
   }
 `;
